d4e48faf8124dc29848c8cc6bd5154a76a744088
Tyler Lovelace <tlovelac01@gmail.com>
2021-03-13 14:33:00 -0500

    change default lambda, fix print override

--- R/MiscFunctions.R
+++ R/MiscFunctions.R
@@ -1,16 +1,17 @@
 #' A print override function for the graph
 #'
-#' @param g The graph object
+#' @param x The graph object
 #' @export
-print.graph <- function(g) {
-    cat("Graph generated using algorithm: ", g[["algorithm"]], "\n")
-    cat("Num nodes: ", length(g[["nodes"]]), "\n")
-    cat("Num edges: ", length(g[["edges"]]), "\n")
+print.graph <- function(x) {
+    cat("Graph generated using algorithm: ", x[["algorithm"]], "\n")
+    cat("Num nodes: ", length(x[["nodes"]]), "\n")
+    cat("Num edges: ", length(x[["edges"]]), "\n")
     # TODO - number of directed and instability
-    cat("Num undirected edges: ", length(which(grepl("---", g[["edges"]], fixed=TRUE))), "\n")
-    cat("Num directed edges: ", length(which(grepl("-->", g[["edges"]], fixed=TRUE))), "\n")
-    cat("Num bidirected edges: ", length(which(grepl("---", g[["edges"]], fixed=TRUE))), "\n")
+    cat("Num undirected edges: ", length(which(grepl("---", x[["edges"]], fixed=TRUE))), "\n")
+    cat("Num directed edges: ", length(which(grepl("-->", x[["edges"]], fixed=TRUE))), "\n")
+    cat("Num bidirected edges: ", length(which(grepl("<->", x[["edges"]], fixed=TRUE))), "\n")
     if (!is.null(g[["stabilities"]])) {
-        cat("Average instability: ", mean( 2 * g[["stabilities"]] * (1 - g[["stabilities"]]) ), "\n")
+        cat("Average instability: ", mean( 2 * x[["stabilities"]] * (1 - x[["stabilities"]]) ), "\n")
     }
+    invisible(x)
 }
