ea6d402168b207fc9a74bea45b7c37b34e4a95da
Tyler Lovelace <tlovelac01@gmail.com>
2021-03-13 17:32:43 -0500

    change print output

--- R/MiscFunctions.R
+++ R/MiscFunctions.R
@@ -3,13 +3,13 @@
 #' @param x The graph object
 #' @export
 print.graph <- function(x) {
-    cat("Graph generated using algorithm: ", x[["algorithm"]], "\n")
-    cat("Num nodes: ", length(x[["nodes"]]), "\n")
-    cat("Num edges: ", length(x[["edges"]]), "\n")
+    cat("Algorithm: ", x[["algorithm"]], "\n")
+    cat("Nodes: ", length(x[["nodes"]]), "\n")
+    cat("Edges: ", length(x[["edges"]]), "\n")
     # TODO - number of directed and instability
-    cat("Num undirected edges: ", length(which(grepl("---", x[["edges"]], fixed=TRUE))), "\n")
-    cat("Num directed edges: ", length(which(grepl("-->", x[["edges"]], fixed=TRUE))), "\n")
-    cat("Num bidirected edges: ", length(which(grepl("<->", x[["edges"]], fixed=TRUE))), "\n")
+    cat("  Undirected: ", length(which(grepl("---", x[["edges"]], fixed=TRUE))), "\n")
+    cat("  Directed: ", length(which(grepl("-->", x[["edges"]], fixed=TRUE))), "\n")
+    cat("  Bidirected: ", length(which(grepl("<->", x[["edges"]], fixed=TRUE))), "\n")
     if (!is.null(x[["stabilities"]])) {
         cat("Average instability: ", mean( 2 * x[["stabilities"]] * (1 - x[["stabilities"]]) ), "\n")
     }
--- src/SearchWrappers.cpp
+++ src/SearchWrappers.cpp
@@ -46,8 +46,6 @@ Rcpp::List mgm(
 
     return result;
 }
-
-// Rcpp::NumericVector::create(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85),
  
 //' Calculate the optimal lambda values for the MGM algorithm and run the algorithm using those values. Optimal values are printed
 //'
