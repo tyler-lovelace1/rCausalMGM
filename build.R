d9bbb038509090b20f468541421b4779467f945e
tyler-lovelace1 <tyl15@pitt.edu>
2022-09-21 02:04:14 -0400

    bug fix with shallow copy of NumericVectors

--- build.R
+++
@@ -1,3 +0,0 @@
-library(RcppArmadillo)
-Rcpp::compileAttributes(".")
-
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-R CMD BATCH build.R
-# R CMD build .
-# R CMD INSTALL rCausalMGM_1.0.tar.gz
-R CMD INSTALL .
-
-Rscript test/test_MGM.R # > ../test_results/C++debug.txt
